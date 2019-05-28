e1f7ff4094d7367a28179e58ee7c275e3bf2789c
nonoSword <chensijie08@gmail.com>
2019-05-28 17:56:41 +0800

    update usercenter.models add blank

--- apps/usercenter/models.py
+++ apps/usercenter/models.py
@@ -6,16 +6,16 @@ from datetime import datetime
 
 # Create your models here.
 class User(AbstractUser):
-    birthday = models.DateField(verbose_name='生日')
-    uuid = models.IntegerField(verbose_name='uuid')
+    birthday = models.DateField(verbose_name='生日', null=True, blank=True)
+    uuid = models.IntegerField(verbose_name='uuid', null=True, blank=True)
     sex = models.CharField(max_length=10, choices=(('male', '男'), ('female', '女'), ('secrete', '保密')),
                            default='female', verbose_name='性别')
-    description = models.TextField(max_length=2048, verbose_name='描述')
-    ip_last_login = models.CharField(max_length=128, verbose_name='最后登录IP')
-    ip_joined = models.CharField(max_length=128, verbose_name='注册IP')
-    date_last_login = models.DateField(default=datetime.now)
+    description = models.TextField(max_length=2048, verbose_name='描述', null=True, blank=True)
+    ip_last_login = models.CharField(max_length=128, verbose_name='最后登录IP', null=True, blank=True)
+    ip_joined = models.CharField(max_length=128, verbose_name='注册IP', null=True, blank=True)
+    date_last_login = models.DateField(default=datetime.now, null=True, blank=True)
     avatar = models.ImageField(upload_to='avatar/%Y/%m', default='avatar/default/male.jpg', verbose_name='用户头像')
-    qq = models.IntegerField(verbose_name='QQ')
+    qq = models.IntegerField(verbose_name='QQ', null=True, blank=True)
 
 
     class Meta:
