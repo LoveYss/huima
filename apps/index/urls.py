b8cb95917217f00ce4157d528ac573bdbaddfaee
LoveYss <Wjl520Yss@163.com>
2019-05-29 13:13:16 +0800

    update index.html

--- apps/index/urls.py
+++ apps/index/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from index.views import *
 from django.urls import re_path
 
 urlpatterns = [
-    # path('', admin.site.urls),
+    path('', show_index, name='index'),
 ]
--- apps/index/views.py
+++ apps/index/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from index.models import Banner
 
+
+def show_index(request):
+    banners_img = Banner.objects.all()
+    context = {
+        'banners_img': banners_img
+    }
+    return render(request, 'index/index.html', context)
 # Create your views here.
