d2b4632dcfe73ee69e526e6e03189c52c2c43515
nonoSword <chensijie08@gmail.com>
2019-05-29 17:52:29 +0800

    fix form bug

--- apps/usercenter/urls.py
+++ apps/usercenter/urls.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path, re_path
-from usercenter.views import UserCenter
+from usercenter.views import *
 
 urlpatterns = [
 
     path('<int:uid>/', UserCenter.as_view(), name='个人中心'),
 
-    path('submit_userinfo_update/', )
+    path('submit_userinfo_update/', UserInfoUpdateForm.as_view())
 
 ]
 
