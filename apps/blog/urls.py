3bad0785c19676fe254662dd5128be925cbee547
zw <1126576700@qq.com>
2019-05-29 16:56:45 +0800

    creat urls.py and views.py->app blog

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
 
+from blog.views import BlogView, BlogListView
+
 urlpatterns = [
     # path('', admin.site.urls),
+    path('', BlogListView.as_view),
+    re_path(r'^blog/$', BlogView.as_view),
 ]
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -2,9 +2,25 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.views import View
+from blog.models import Blog, BlogComment
+from django.db.models import Q
+
+
+class BlogListView(View):
+    def get(self, request):
+        blogs = Blog.objects.all().order_by('-publish')
+        return render(request, 'blog/bloglist.html', locals())
+
+    # def catalog(self, request):
+    #     type = request.GET.get('type')
+    #     language = request.GET.get('language')
+    #     blogs = Blog.objects.filter(Q(type=type)|Q(language=language))
+    #     return render(request, 'blog/bloglist.html', locals())
 
 
 class BlogView(View):
 
-    def get(self):
-        pass
+    def get(self, request, title):
+        title = request.GET.get("title")
+        blog = Blog.objects.get(title=title)
+        return render(request, 'blog/blog.html', locals())
