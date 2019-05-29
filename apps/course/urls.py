80cec536b0f0812becac531458d1621c165c2ba2
zxr <271055687@qq.com>
2019-05-29 09:57:43 +0800

    course 模块路由配置以及静态模板引入

--- apps/course/urls.py
+++ apps/course/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
+from .views import *
 
 urlpatterns = [
-    # path('', admin.site.urls),
+    path('', show_course, name='course'),
+    path('chapter/', show_chapter, name='chapter'),
+    path('chapter_details/', show_chapter_details, name='chapter_details'),
+
 ]
--- apps/course/views.py
+++ apps/course/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def show_course(request):
+    """
+    课程展示页
+    """
+    render(request, 'course/courseList.html', {'data': ''})
+
+
+def show_chapter(request):
+    render(request, 'course/chapterList.html', {'data': ''})
+
+
+def show_chapter_details(request):
+    render(request, 'course/chapterDetails.html', {'data': ''})
+
