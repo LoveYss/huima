70d13d7a8789c0205606f2c4036333987f93ad76
nonoSword <chensijie08@gmail.com>
2019-05-29 17:47:53 +0800

    update usercenter form

--- apps/usercenter/views.py
+++ apps/usercenter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import View
+from django import forms
 from usercenter.models import User
 from blog.models import *
 
@@ -9,4 +10,18 @@ class UserCenter(View):
     def get(self, request, uid):
         user = User.objects.get(id=uid)
         blog = Blog.objects.get(user_id=uid)
+        author = User.objects.get(id=blog.user_id)
         return render(request, 'usercenter/usercenter.html', locals())
+
+
+class UserInfoUpdateForm(View):
+    def get(self, request):
+        form_obj = UserInfoUpdate()
+        return render(request, 'usercenter/usercenter.html', locals())
+
+
+
+class UserInfoUpdate(forms.Form):
+    username = forms.CharField(label='昵称')
+    sex = forms.CharField(label='性别')
+    email = forms.EmailField(label='邮箱')
