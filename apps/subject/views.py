3e6b4984e0015d8c9de949cd20bf2cadf05dbb6b
xhw <xhw807602352@163.com>
2019-05-29 14:46:09 +0800

    right import models.py

--- apps/subject/views.py
+++ apps/subject/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from apps.subject.models import ProjectDetail
+from subject.models import ProjectDetail
 
 # Create your views here.
 def project_list(request):
