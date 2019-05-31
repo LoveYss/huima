038ef35e5ad0b1fe8277fe3cdfe0f35c904d5d73
nonoSword <chensijie08@gmail.com>
2019-05-31 21:46:22 +0800

    update form

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -2,13 +2,11 @@
 # -*- coding: utf-8 -*-
 
 from django.urls import path, re_path
-from user.views import UserCenter, UserInfoUpdateForm
+from user.views import UserCenter
 
 urlpatterns = [
 
     path('<int:uid>/', UserCenter.as_view(), name='usercenter'),
-
-    path('submit_userinfo_update/', UserInfoUpdateForm.as_view())
 
 ]
 
