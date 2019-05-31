a0e2d1f77006d1c13bc2819bde13a470469e8f31
xhw <xhw807602352@163.com>
2019-05-31 15:37:52 +0800

    update subject/urls.py

--- apps/subject/urls.py
+++ apps/subject/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from django.urls import re_path
-from .views import project_list, project_detail
+from .views import Project_list, Project_detail
 
 urlpatterns = [
     # path('', admin.site.urls),
-    path("", project_list, name="project_list"),
-    path("project_detail", project_detail, name="project_list")
+    # re_path("project_list/(?P<page>)", Project_list.as_view(), name="project_list"),
+    path("project/<int:page>", Project_list.as_view(), name="project_list"),
+    path("project_detail/<name>", Project_detail.as_view(), name="project_detail")
 ]
