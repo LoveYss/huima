6059d16daf74b66874347d715e244de4bc542edf
LoveYss <Wjl520Yss@163.com>
2019-05-31 16:01:18 +0800

    updata login

---
+++ apps/index/forms.py
@@ -0,0 +1,14 @@
+# Author:Loveyss
+# -*-coding:utf-8 -*-
+# @Time     :2019/5/31   14:50
+# @Author   :Loveyss
+# @Site     :
+# @File     :forms.py
+# @Software :PyCharm
+
+from django import forms
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(required=True)
+    password = forms.CharField(required=True, min_length=6)
